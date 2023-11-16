bfd55999e3146e5234227857fa192c669a5628f7
LuisEspinoza <luchito.espinoza11@gmail.com>
2023-11-15 23:24:49 -0400

    feat: headerandfooter

---
+++ src/components/customer-page/customer-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'pet-paradise-client-customer-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './customer-page.component.html',
+  styleUrls: ['./customer-page.component.scss']
+})
+export class CustomerPageComponent {}

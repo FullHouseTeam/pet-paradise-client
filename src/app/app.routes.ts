8c6a23efb15fa387ce14253f0df15fd5e2be4ac5
LuisEspinoza <luchito.espinoza11@gmail.com>
2023-11-19 23:55:54 -0400

    feat: loginpage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { CustomerPageComponent } from '../components/customer-page/customer-page
 import { AboutUsComponent } from '../components/about-us/about-us.component';
 import { SupportPageComponent } from '../components/support-page/support-page.component';
 import { TermsAndConditionsComponent } from '../components/terms-and-conditions/terms-and-conditions.component';
+import { LogInComponent} from "../components/log-in/log-in.component";
+
 export const appRoutes: Route[] = [
   {
     path: 'categories-and-products',
@@ -16,5 +18,6 @@ export const appRoutes: Route[] = [
   { path: 'about-us', component: AboutUsComponent },
   { path: 'support', component: SupportPageComponent },
   { path: 'terms-and-conditions', component: TermsAndConditionsComponent },
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'log-in', component: LogInComponent}
 ];
---
+++ src/components/log-in/log-in.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from "@angular/material/input";
+import { NgIf } from "@angular/common";
+import { FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+
+@Component({
+  selector: 'pet-paradise-client-log-in',
+  standalone: true,
+  imports: [CommonModule, MatButtonModule, MatDividerModule, MatIconModule, MatFormFieldModule, NgIf, FormsModule, ReactiveFormsModule, MatInputModule],
+  templateUrl: './log-in.component.html',
+  styleUrls: ['./log-in.component.scss']
+})
+export class LogInComponent {
+  email = new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9]{1,20}@[a-zA-Z0-9]{1,20}\\.[a-zA-Z0-9]{1,20}$')]);
+  password = new FormControl('');
+  getErrorMessageEmail() {
+    if (this.email.invalid && (this.email.dirty || this.email.touched)) {
+      if (this.email.hasError('required')) {
+        return 'You must enter an email address';
+      }
+      return this.email.hasError('pattern') ? 'Not a valid email address' : '';
+    }
+    return '';
+  }
+
+
+  hide = true;
+
+}

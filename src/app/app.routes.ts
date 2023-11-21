7b204184cd5a671a9121245838ea351712e67516
LuisEspinoza <luchito.espinoza11@gmail.com>
2023-11-21 16:03:56 -0400

    feat: product info component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { ShopCartComponent } from '../components/shop-cart/shop-cart.component';
 import { SupportPageComponent } from '../components/support-page/support-page.component';
 import { TermsAndConditionsComponent } from '../components/terms-and-conditions/terms-and-conditions.component';
 import { DescriptionComponent } from '../components/example-api/description.component';
+import { ProductInfoComponent } from "../components/product-info/product-info/product-info.component";
 
 export const appRoutes: Route[] = [
   { path: 'store', component: CategoriesAndProductsComponent },
@@ -17,4 +18,6 @@ export const appRoutes: Route[] = [
   { path: 'terms-and-conditions', component: TermsAndConditionsComponent },
   { path: '', component: HomeComponent },
   { path: '**', component: DescriptionComponent },
-  ];
+  { path: 'example-api', component: DescriptionComponent },
+  { path: 'product/:id', component: ProductInfoComponent }
+];

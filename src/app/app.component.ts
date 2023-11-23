8d0ff872c0e8158949d5da7bf83fcacf2b63f19d
BiancaSoliz <biancasoliz258@gmail.com>
2023-11-23 12:45:38 -0400

    feat: add bootstrap
    Added bootstrap to speed up the layout of the views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from "@angular/core";
 import { Router, RouterModule } from '@angular/router';
-import { HeaderComponent } from './shared/header/header.component';
-import { HomeComponent } from '../components/home/home.component';
-import { FooterComponent } from '../components/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component'
+import { HomeComponent } from "./pages/home/home.component";
+import { FooterComponent } from "./shared/footer/footer.component";
 
 @Component({
   standalone: true,

f486720545f4f5d79db0b29df3d4a1f0ac1e7cd6
Hugo Oropeza <oropezahugo35@gmail.com>
2023-11-23 08:08:45 -0400

    feat:categories-view

--- src/components/categories-and-products/categories-and-products.component.ts
+++ src/components/categories-and-products/categories-and-products.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {ProductCardComponent} from "../product-card/product-card.component";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {CdkFixedSizeVirtualScroll} from "@angular/cdk/scrolling";
 
 @Component({
   selector: 'pet-paradise-client-categories-and-products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ProductCardComponent, MatButtonToggleModule, MatButtonModule, MatInputModule, MatSelectModule, CdkFixedSizeVirtualScroll],
   templateUrl: './categories-and-products.component.html',
   styleUrls: ['./categories-and-products.component.scss']
 })

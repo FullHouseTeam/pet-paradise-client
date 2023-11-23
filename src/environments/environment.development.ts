4f965cf7af787891643dafca0612f881f9931be3
José Luis Terán <joseluisteran2617@gmail.com>
2023-11-23 15:40:14 -0400

    chore: update backend url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5155'
+  rest_api_url: 'http://rest-api-pet-paradise.westus3.cloudapp.azure.com/swagger/index.html'
 };

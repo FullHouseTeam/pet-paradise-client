2bed70988b1f873383e8d7356d161131fd99d779
Hugo Oropeza <oropezahugo35@gmail.com>
2023-11-24 19:08:57 -0400

    feat:add-redirects-responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -29,6 +29,13 @@ export const appRoutes: Route[] = [
   { path: 'categories-and-products/reptile', component: CategoriesAndProductsComponent },
   { path: '**', component: DescriptionComponent },
   { path: 'example-api', component: DescriptionComponent },
-  { path: 'product/:id', component: ProductInfoComponent }
+  { path: 'product/:id', component: ProductInfoComponent },
+  { path: '', component: HomeComponent },
+  { path: 'categories-and-products/dog', component: CategoriesAndProductsComponent },
+  { path: 'categories-and-products/cat', component: CategoriesAndProductsComponent },
+  { path: 'categories-and-products/parrot', component: CategoriesAndProductsComponent },
+  { path: 'categories-and-products/fish', component: CategoriesAndProductsComponent },
+  { path: 'categories-and-products/rodent', component: CategoriesAndProductsComponent },
+  { path: 'categories-and-products/reptile', component: CategoriesAndProductsComponent }
 ];
 

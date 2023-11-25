b942f20fd6790d697f8e85a3932b2771e8b31801
José Luis Terán <joseluisteran2617@gmail.com>
2023-11-25 16:49:59 -0400

    refactor: configure TestBed to use the HttpClientModule and inject to each Services

--- src/services/purchases/purchase.service.spec.ts
+++ src/services/purchases/purchase.service.spec.ts
@@ -1,12 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-
 import { PurchaseService } from './purchase.service';
 
 describe('PurchaseService', () => {
   let service: PurchaseService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(PurchaseService);
   });
 

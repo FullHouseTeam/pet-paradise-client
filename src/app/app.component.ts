4844f46e598ec65a400e34c96b524a8c45cb7771
BiancaSoliz <biancasoliz258@gmail.com>
2023-11-26 11:54:29 -0400

    fix: Header refactor without bootstrap.
    header refactored without bootstrap with only html and scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from "@angular/core";
 import { Router, RouterModule } from "@angular/router";
-import { HomeComponent } from "./pages/home/home.component";
-import { FooterComponent } from "./shared/footer/footer.component";
 import { HeaderComponent } from "./shared/header/header.component";
+import { HomeComponent } from "../components/home/home.component";
+import { FooterComponent } from "../components/footer/footer.component";
 
 @Component({
   standalone: true,
@@ -17,10 +17,6 @@ export class AppComponent {
   title = "pet-paradise-client";
 
   showHeaderFooter() {
-    console.log(
-      ["/categories-and-products", "/shop-cart", "/home", "/terms-and-conditions", "/support", "/store", "/about", "/sign-up", "/login"].includes(this.router.url),
-      this.router.url
-    );
-    return ["/categories-and-products", "/shop-cart", "/home", "/terms-and-conditions", "/support", "/store", "/about", "/sign-up", "/login"].includes(this.router.url);
+    return ["/", "/categories-and-products", "/shop-cart", "/home", "/terms-and-conditions", "/support", "/store", "/about", "/sign-up", "/login"].includes(this.router.url);
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Route } from '@angular/router';
-import { HomeComponent } from '../components/home/home.component';
-import { ShopCartComponent } from '../components/shop-cart/shop-cart.component';
+import { AboutUsComponent } from '../components/about-us/about-us.component';
 import { CategoriesAndProductsComponent } from '../components/categories-and-products/categories-and-products.component';
 import { CustomerPageComponent } from '../components/customer-page/customer-page.component';
-import { AboutUsComponent } from '../components/about-us/about-us.component';
+import { HomeComponent } from '../components/home/home.component';
+import { ShopCartComponent } from '../components/shop-cart/shop-cart.component';
 import { SupportPageComponent } from '../components/support-page/support-page.component';
 import { TermsAndConditionsComponent } from '../components/terms-and-conditions/terms-and-conditions.component';
-import { DescriptionComponent } from '../components/example-api/description.component';
+
 export const appRoutes: Route[] = [
   { path: 'store', component: CategoriesAndProductsComponent },
   { path: 'shop-cart', component: ShopCartComponent },
@@ -15,5 +15,4 @@ export const appRoutes: Route[] = [
   { path: 'support', component: SupportPageComponent },
   { path: 'terms-and-conditions', component: TermsAndConditionsComponent },
   { path: '', component: HomeComponent },
-  { path: 'example-api', component: DescriptionComponent }
-];
+  ];

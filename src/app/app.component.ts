85544786d99dcd07be89caa20b641f781014f365
BiancaSoliz <biancasoliz258@gmail.com>
2023-11-26 15:10:52 -0400

    fix: corrected header based on comments
    header scss styles are handled inside the component, single quotes handling, removal of repeated icon library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,20 @@
 import { Component } from "@angular/core";
-import { Router, RouterModule } from "@angular/router";
-import { HeaderComponent } from "./shared/header/header.component";
-import { HomeComponent } from "../components/home/home.component";
-import { FooterComponent } from "../components/footer/footer.component";
+import { Router, RouterModule } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { HomeComponent } from '../components/home/home.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   standalone: true,
   imports: [HomeComponent, RouterModule, HeaderComponent, FooterComponent],
-  selector: "pet-paradise-client-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  selector: 'pet-paradise-client-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   constructor(private router: Router) {}
 
-  title = "pet-paradise-client";
+  title = 'pet-paradise-client';
 
   showHeaderFooter() {
     return ["/", "/categories-and-products", "/shop-cart", "/home", "/terms-and-conditions", "/support", "/store", "/about", "/sign-up", "/login"].includes(this.router.url);

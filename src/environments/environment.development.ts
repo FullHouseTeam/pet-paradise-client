5958cd45920614c9bc72b65ad0a5c0ce7ac01ab2
Hugo Oropeza <oropezahugo35@gmail.com>
2023-11-26 17:52:33 -0400

    feat:teke out non correspondent add

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  rest_api_url: 'http://rest-api-pet-paradise.westus3.cloudapp.azure.com/'
+  rest_api_url: 'http://rest-api-pet-paradise.westus3.cloudapp.azure.com/swagger/index.html'
 };

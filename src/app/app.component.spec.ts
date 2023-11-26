2f1ae66f83cc234fa2f3470ed4f0126e00223818
BiancaSoliz <biancasoliz258@gmail.com>
2023-11-26 15:10:52 -0400

    fix: corrected header based on comments
    header scss styles are handled inside the component, single quotes handling, removal of repeated icon library

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HomeComponent } from './pages/home/home.component';
+import { HomeComponent } from '../components/home/home.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {

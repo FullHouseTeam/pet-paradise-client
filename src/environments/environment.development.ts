cde2cd85423ff20fc8d840da613904a951e7ce75
José Luis Terán <joseluisteran2617@gmail.com>
2023-11-26 11:30:03 -0400

    refactor: update API url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  rest_api_url: 'http://rest-api-pet-paradise.westus3.cloudapp.azure.com/swagger/index.html'
+  rest_api_url: 'http://rest-api-pet-paradise.westus3.cloudapp.azure.com/'
 };

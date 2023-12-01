e9b6184131801cf6d8df4ecea0c4f07cf37fb7f7
Luis Espinoza <luchito.espinoza11@gmail.com>
2023-12-01 13:46:56 -0400

    fix: unused conflictive imports removed

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {CategoriesContainerComponent} from "../categories-container/categories-container.component";
-import {SlickCarouselModule} from "ngx-slick-carousel";
 import {ProductCardComponent} from "../product-card/product-card.component";
 import {CustomCarouselComponent} from "./custom-carousel/custom-carousel.component";
 import {ProductService} from "../../services/products/product.service";
@@ -11,7 +10,7 @@ import {forkJoin} from "rxjs";
 @Component({
   selector: 'pet-paradise-client-home',
   standalone: true,
-  imports: [CommonModule, CategoriesContainerComponent, SlickCarouselModule, ProductCardComponent, CustomCarouselComponent],
+  imports: [CommonModule, CategoriesContainerComponent, ProductCardComponent, CustomCarouselComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })

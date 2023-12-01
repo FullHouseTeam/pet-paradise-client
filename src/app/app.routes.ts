823f4e14659933f637e4146f5ee2021312172a6a
José Luis Terán <joseluisteran2617@gmail.com>
2023-12-01 15:18:16 -0400

    chore: add product per id route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,17 +2,18 @@ import { Route } from '@angular/router';
 import { AboutUsComponent } from '../components/about-us/about-us.component';
 import { CategoriesAndProductsComponent } from '../components/categories-and-products/categories-and-products.component';
 import { CustomerPageComponent } from '../components/customer-page/customer-page.component';
+import { DescriptionComponent } from '../components/example-api/description.component';
 import { HomeComponent } from '../components/home/home.component';
+import { LoginComponent } from "../components/login/login.component";
+import { ProductInfoComponent } from "../components/product-info/product-info.component";
 import { ShopCartComponent } from '../components/shop-cart/shop-cart.component';
 import { SupportPageComponent } from '../components/support-page/support-page.component';
 import { TermsAndConditionsComponent } from '../components/terms-and-conditions/terms-and-conditions.component';
-import { DescriptionComponent } from '../components/example-api/description.component';
-import { ProductInfoComponent } from "../components/product-info/product-info.component";
-import { LoginComponent } from "../components/login/login.component";
 
 export const appRoutes: Route[] = [
   { path: 'login', component: LoginComponent },
   { path: 'store', component: CategoriesAndProductsComponent },
+  { path: 'product/:id', component: ProductInfoComponent},
   { path: 'shop-cart', component: ShopCartComponent },
   { path: 'customer', component: CustomerPageComponent },
   { path: 'about', component: AboutUsComponent },

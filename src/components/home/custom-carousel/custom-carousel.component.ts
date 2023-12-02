be19493b609f7dc4ee2eb4b8b1cd3826e969a13e
Hugo Oropeza <oropezahugo35@gmail.com>
2023-12-02 13:15:57 -0400

    fix: random products on home page carousels

--- src/components/home/custom-carousel/custom-carousel.component.ts
+++ src/components/home/custom-carousel/custom-carousel.component.ts
@@ -13,6 +13,7 @@ import {MatIconModule} from "@angular/material/icon";
 })
 export class CustomCarouselComponent {
   @Input() products: Product[] = [];
+  productsShuffled: Product[] = [];
   productsToShow: Product[] = [];
   itemsPerGroup = 4;
   currentIndex = 0;
@@ -20,14 +21,16 @@ export class CustomCarouselComponent {
 
 
   ngOnInit() {
+    this.productsShuffled = this.products.slice()
+    this.productsShuffled = this.shuffleProducts(this.productsShuffled);
     this.adjustAttributeBasedOnWindowSize();
     this.showData();
   }
 
   showMore() {
-    if (this.currentIndex + this.itemsPerGroup < this.products.length) {
-      this.productsToShow = this.products.slice(this.currentIndex + this.itemsPerGroup, this.currentIndex + this.itemsPerGroup * 2);
-      if ((this.currentIndex + this.itemsPerGroup) < this.products.length) {
+    if (this.currentIndex + this.itemsPerGroup < this.productsShuffled.length) {
+      this.productsToShow = this.productsShuffled.slice(this.currentIndex + this.itemsPerGroup, this.currentIndex + this.itemsPerGroup * 2);
+      if ((this.currentIndex + this.itemsPerGroup) < this.productsShuffled.length) {
         this.currentIndex = this.currentIndex + this.itemsPerGroup
       }
     }
@@ -35,12 +38,12 @@ export class CustomCarouselComponent {
   }
 
   showData() {
-      this.productsToShow = this.products.slice(this.currentIndex, this.itemsPerGroup);
+    this.productsToShow = this.productsShuffled.slice(this.currentIndex, this.itemsPerGroup);
   }
 
   showLess() {
     if (this.currentIndex - this.itemsPerGroup >= 0) {
-      this.productsToShow = this.products.slice(this.currentIndex - this.itemsPerGroup, this.currentIndex);
+      this.productsToShow = this.productsShuffled.slice(this.currentIndex - this.itemsPerGroup, this.currentIndex);
       if ((this.currentIndex - this.itemsPerGroup ) >= 0) {
         this.currentIndex -= this.itemsPerGroup;
       }
@@ -53,6 +56,16 @@ export class CustomCarouselComponent {
     this.currentIndex = 0;
   }
 
+  shuffleProducts(products: Product[]) {
+
+    for (let i = products.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [products[i], products[j]] = [products[j], products[i]];
+
+    }
+    console.log(products)
+    return products;
+  }
 
   @HostListener('window:resize', ['$event'])
   onResize(event: Event) {
